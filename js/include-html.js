e54a8f6815fcd66962cd2141ca67c2981c47f5e9
web-tinkering <webtinkering@gmail.com>
2021-02-06 00:23:44 +0530

    checking errors on live page

---
+++ js/include-html.js
@@ -0,0 +1,7 @@
+$(function(){
+    $("#load-navbar").load("./random-folder/navbar.html");
+});
+
+$(function(){
+    $("#load-contact-and-subscribe").load("./random-folder/contact-and-subscribe.html");
+});
